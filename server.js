f72222a01a964297ac4a9a72976230ef91c75257
Giochi500 <finance@mega.com.ge>
2026-08-21 16:51:58 +0400

    Update data folder path to use OneDrive location

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-const SALARY_DIR = path.join(__dirname, 'data');
+const SALARY_DIR = 'C:\\Users\\Legion\\OneDrive\\დოკუმენტები\\My app\\data';
 
 // PostgreSQL connection
 const pool = new Pool({
